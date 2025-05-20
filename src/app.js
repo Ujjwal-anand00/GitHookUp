27ec8bf8babaee114676585abf0ae544528259c2
Ujjwal Anand <ujjwal.anand6376@gmail.com>
2025-05-20 11:11:45 +0530

    Server is created using express js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.use("/hello",(req,res)=>{
+    res.send("Hello , HELLO");
+
+});
+
+app.use("/hi",(req,res)=>{
+    res.send("Hello , HI");
+
+});
+app.use((req,res)=>{
+    res.send("Hello from the server");
+
+});
+app.listen(3000 , () => {
+    console.log('Server is running on port 3000');
+});
