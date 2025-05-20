520192ffbdc5770c468f41795070da1a125fbd58
Ujjwal Anand <ujjwal.anand6376@gmail.com>
2025-05-20 11:28:44 +0530

    Learning Routing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+app.use("/",(req,res)=>{
+    res.send("Hello Ujjwal");
+
+});
 app.use("/hello",(req,res)=>{
-    res.send("Hello , HELLO");
+    res.send("Hello Hello Hello");
 
 });
 
