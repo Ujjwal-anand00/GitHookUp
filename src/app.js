d51c767fcc7fc6d0f690056e0613935f4742a45f
Ujjwal Anand <ujjwal.anand6376@gmail.com>
2025-06-07 16:32:25 +0530

    Learning HTTP routes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const app = express();
-app.use("/",(req,res)=>{
-    res.send("Hello Ujjwal");
 
+// it will handle only get call to the /user endpoint
+app.get("/user",(req,res)=>{
+    res.send({FirstName:"Ujjwal", LastName: "Anand"}); 
+})
+// it will handle only post call to the /user endpoint
+app.post("/user",(req,res)=>{
+    console.log("Save data to the database");
+    res.send("User data saved");
 });
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello");
-
+// it will handle only delete call to the /user endpoint
+app.delete("/user",(req,res)=>{
+    // delete user data from the database
+    res.send("User data deleted");
 });
 
-app.use("/hi",(req,res)=>{
-    res.send("Hello , HI");
-
-});
-app.use((req,res)=>{
-    res.send("Hello from the server");
+app.use("/test",(req,res)=>{
+    res.send("Hello from the user");
 
 });
 app.listen(3000 , () => {
